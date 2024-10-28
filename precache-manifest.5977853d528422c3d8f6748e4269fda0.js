9a8c3d497cfc33b683b89babe42f78a789132141
gigamac <gigamac@users.noreply.github.com>
2024-10-28 16:09:37 +0000

    deploy: 5713dc5cc13a571c897d2169e76bb46aafe3bd50

---
+++ precache-manifest.5977853d528422c3d8f6748e4269fda0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b28bf5dcb00d400fe08fde52d0de411",
+    "url": "https://gigamac.github.io/task-management-frontend-2024-10-25/index.html"
+  },
+  {
+    "revision": "49a9b713abba12124c0b",
+    "url": "https://gigamac.github.io/task-management-frontend-2024-10-25/static/css/main.902b4043.chunk.css"
+  },
+  {
+    "revision": "f6a2cff5589106665a61",
+    "url": "https://gigamac.github.io/task-management-frontend-2024-10-25/static/js/2.1a5b130f.chunk.js"
+  },
+  {
+    "revision": "49a9b713abba12124c0b",
+    "url": "https://gigamac.github.io/task-management-frontend-2024-10-25/static/js/main.c8cf20e2.chunk.js"
+  },
+  {
+    "revision": "715ac6cd58cabdf14a83",
+    "url": "https://gigamac.github.io/task-management-frontend-2024-10-25/static/js/runtime~main.8a042dd6.js"
+  }
+]);
